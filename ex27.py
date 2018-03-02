eacdab7b3ed8e76dbdec7e1d50865cbddaa94407
itmanharry <jack_chengqian@163.com>
2018-03-02 15:01:31 +0800

    Add a new Python file

---
+++ ex27.py
@@ -0,0 +1,15 @@
+# _*_ coding:utf-8 _*_
+b = True and False
+print "1." , b
+b = False and True
+print "2.", b
+b = 1 == 1 and 2 == 1
+print "3.", b
+b = "test" == "test"
+print "4.", b
+b = 1 == 1 or 2 != 1
+print "5.", b
+b = True and 1 == 1
+print "6.", b
+b = False and 0!= 0
+print "7.", b
