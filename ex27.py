397af9fb0374426e08a7ecd64e0e65da5e795923
itmanharry <jack_chengqian@163.com>
2018-03-02 15:28:48 +0800

    Modify the ex17.py file

--- ex27.py
+++ ex27.py
@@ -13,3 +13,29 @@ b = True and 1 == 1
 print "6.", b
 b = False and 0!= 0
 print "7.", b
+b = True or 1 == 1
+print "8.", b
+b = "test" == "testing"
+print "9.", b
+b = 1!= 0 and 2 == 1
+print "10.", b
+b = "test" != "testing"
+print "11.", b
+b = "test" == 1
+print "12.", b
+b = not(True and False)
+print "13.", b
+b = not(1 == 1 and 0 != 1)
+print "14.", b
+b = not(10 == 1 or 1000 == 1000)
+print "15.", b
+b = not(1 != 10 or 3 == 4)
+print "16.", b
+b = not("testing" == "testing" and "Zed" == "Cool guy")
+print "17.", b
+b = 1 == 1 and not("testing" == 1 or 1 == 0)
+print "18.", b
+b = "chunky" == "bacon" and not(3 == 4 or 3 == 3)
+print "19.", b
+b = 3 == 3 and not("testing" == "testing" or "Python" == "Fun")
+print "20.", b
