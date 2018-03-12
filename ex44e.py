4e17911ff220b499ccee16bcb7f45b4d6f94f6d5
itmanharry <jack_chengqian@163.com>
2018-03-12 15:44:30 +0800

    add a 44e.py file

---
+++ ex44e.py
@@ -0,0 +1,22 @@
+class Other(object):
+	def override(self):
+		print "Other override()"
+	def implicit(self):
+		print "Other implicit()"
+	def altered(self):
+		print "Other altered()"
+class Child(object):
+	def __init__(self):
+		self.other = Other()
+	def implicit(self):
+		self.other.implicit()
+	def override(self):
+		print "Child override()"
+	def altered(self):
+		print "Child, before the other altered()"
+		self.other.altered()
+		print "Child, after the other altered()"
+son = Child()
+son.implicit()
+son.override()
+son.altered()
