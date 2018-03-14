b7dcb4ccb30c40d83aaeb915db45b9d5e012dfd9
itmanharry <jack_chengqian@163.com>
2018-03-14 16:31:04 +0800

    Update the skeleton files

---
+++ ProjectSkeleton/projects/skeleton/setup.py
@@ -0,0 +1,17 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+config = [
+	'description':'My First Python Project',
+	'author':'Harry',
+	'url':'URL to get it.',
+	'download_url':'Where to download it.',
+	'author_email':'jack_chengqian@163.com',
+	'version':'0.1',
+	'install_requaires':['nose'],
+	'packages':['NAME'],
+	'scripts':[],
+	'name':'FP',
+]
+setup(**config)
