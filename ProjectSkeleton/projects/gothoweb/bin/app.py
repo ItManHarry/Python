c6b2b802441c2ff84a32d29627c9d1710e5510b0
itmanharry <jack_chengqian@163.com>
2018-03-20 16:13:08 +0800

    Web modify

--- ProjectSkeleton/projects/gothoweb/bin/app.py
+++ ProjectSkeleton/projects/gothoweb/bin/app.py
@@ -7,6 +7,7 @@ render = web.template.render('templates/')
 class Index(object):
 	def GET(self):
 		greeting = "Hello word"
-		return render.index(greeting=greeting)
+		#return render.index(greeting=greeting)
+		return greeting
 if __name__ == "__main__":
 	app.run()
