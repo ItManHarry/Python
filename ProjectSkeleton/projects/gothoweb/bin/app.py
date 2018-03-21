05e60010c59c989821afe0b460eecf00b4626ac0
itmanharry <jack_chengqian@163.com>
2018-03-21 14:34:38 +0800

    Modify the app.py

--- ProjectSkeleton/projects/gothoweb/bin/app.py
+++ ProjectSkeleton/projects/gothoweb/bin/app.py
@@ -3,11 +3,12 @@ urls = (
 	'/','Index'
 )
 app = web.application(urls, globals())
-render = web.template.render('templates/')
+render = web.template.render("templates/")
 class Index(object):
 	def GET(self):
 		greeting = "Hello word"
-		#return render.index(greeting=greeting)
-		return greeting
+		print greeting
+		return render.index(greeting=greeting)
+		#return greeting
 if __name__ == "__main__":
 	app.run()
