b2e7b5096493317a077a89b05bc0579a3892e41d
itmanharry <jack_chengqian@163.com>
2018-04-10 15:29:23 +0800

    modify the numeric1.py file

--- LearningPython/Python3/Numeric/numeric1.py
+++ LearningPython/Python3/Numeric/numeric1.py
@@ -3,3 +3,38 @@ print(40 + 3.14)
 print(int(3.1415))
 print(float(3))
 print('-' * 80)
+#numeric in action
+a = 3
+b = 4
+print('a is :', a , ' b is :', b)
+print(a + 1, a -1)
+print(b * 3, b / 2)
+print(a % 2, b ** 2)
+print(2 + 4.0, 2.0 ** b)
+print(b / 2+a)
+print(b / (2.0+a))
+print(1 / 2)
+num = 1 / 3
+print(num)
+print('%e' %num)
+print('%4.2f' %num)
+print('{0:4.2f}'.format(num))
+print('-' * 80)
+#comparisons
+print(1 < 2)
+print(2.0 >= 1)
+print(2.0 == 2.0)
+print(2.0 != 2.0)
+x = 2
+y = 4
+z = 6
+print(x < y < z)
+print(x < y and y < z)
+print(x < y > z)
+print(x < y and y > z)
+print(1 < 2 < 3.0 < 4)
+print(1 > 2 > 3.0 > 4)
+print(1 == 2 < 3)
+print(1.1 + 2.2 == 3.3)
+print(int(1.1 + 2.2) == int(3.3))
+print('-' * 80)
