260707bfc06275d137ce3fa0fdf77743a08c96bc
itmanharry <jack_chengqian@163.com>
2018-04-19 15:52:53 +0800

    Modify the string1.py file

--- LearningPython/Python3/String/string1.py
+++ LearningPython/Python3/String/string1.py
@@ -1,3 +1,22 @@
 print('-' * 80)
-
+print('spa"m')
+print("spa'm")
+print('''
+	...spam...
+	...harry...
+''')
+print("""
+	...jack...
+	...sam...
+""")
+print("s\tp\na\0m")
+print(r"c:\new\test.spm")
+print('-' * 80)
+title = "Meaning " 'of' " Life"
+print(title)
+print('knight\'s')
+print("knight\"s")
+s = 'a\nb\tc'
+print(s)
+print(len(s))
 print('-' * 80)
