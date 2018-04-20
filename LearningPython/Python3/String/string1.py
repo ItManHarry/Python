360367de5324b94a287f17c7652fba7a6294bfd4
itmanharry <jack_chengqian@163.com>
2018-04-20 16:18:57 +0800

    finish string1.py file

--- LearningPython/Python3/String/string1.py
+++ LearningPython/Python3/String/string1.py
@@ -19,4 +19,31 @@ print("knight\"s")
 s = 'a\nb\tc'
 print(s)
 print(len(s))
+s = '\001\002\x03'
+print(s)
+print(len(s))
+print('-' * 80)
+x = "c:\py\code"
+print(x)
+print(len(x))
+print('-' * 80)
+#raw text
+path = 'c:\new\text.dat'
+print(path)
+path = r'c:\new\text.dat'
+print(path)
+path = 'c:\\new\\text.dat'
+print(path)
+print('-' * 80)
+mantra = """
+	Always look
+	on the bright
+	side of life.
+"""
+print(mantra)
+menu = """
+	spam # comments here added to string!
+	eggs # ditto
+"""
+print(menu)
 print('-' * 80)
