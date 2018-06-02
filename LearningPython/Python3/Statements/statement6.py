88d721720cf0250709888b32e283f8236ec429d7
ChengGuoQian <jack_chengqian@163.com>
2018-06-02 11:19:51 +0800

    itration comphansion

--- LearningPython/Python3/Statements/statement6.py
+++ LearningPython/Python3/Statements/statement6.py
@@ -11,3 +11,28 @@ print('-' * 80)
 for line in open('statement1.py'):
 	print(line.upper())
 print('-' * 80)
+l = [1,2,3,4]
+print(l)
+for i in range(len(l)):
+	l[i] += 10
+print(l)
+l2 = [x + 10 for x in l]
+print(l2)
+print('-' * 80)
+lines = [line.rstrip() for line in open('statement1.py')]
+print(lines)
+lines = [line.rstrip() for line in open('statement1.py') if line[0] != 'p']
+print(lines)
+print('-' * 80)
+fs = [x+y for x in 'abc' for y in 'xyz']
+print(fs)
+print('-' * 80)
+fls = list(open('statement1.py'))
+print(fls)
+print('-' * 80)
+fts = tuple(open('statement1.py'))
+print(fts)
+print('-' * 80)
+ffs = '&&'.join(open('statement1.py'))
+print(ffs)
+print('-' * 80)
