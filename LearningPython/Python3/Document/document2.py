afc0bd8b264f9fcdbd8c89db56785128a27ef731
itmanharry <jack_chengqian@163.com>
2018-06-13 10:03:24 +0800

    finish statements

---
+++ LearningPython/Python3/Document/document2.py
@@ -0,0 +1,14 @@
+import docstrings
+import sys
+print('-' * 80)
+print(docstrings.__doc__)
+print('-' * 80)
+print(docstrings.square.__doc__)
+print('-' * 80)
+print(docstrings.Employee.__doc__)
+print('-' * 80)
+#help(sys)
+help(docstrings.square)
+print('-' * 80)
+help(docstrings.Employee)
+print('-' * 80)
