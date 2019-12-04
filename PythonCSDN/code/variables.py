91b5bcf69218161d2d71bb35502ce4443b4517a9
itmanharry <jack_chengqian@163.com>
2019-12-04 21:25:46 +0800

    python again

---
+++ PythonCSDN/code/variables.py
@@ -0,0 +1,30 @@
+a = 1
+print("a's value is : " , a)
+print("a is : " , type(a))
+a = "abc"
+print("a's value is : " , a)
+print("a is : " , type(a))
+small = 2
+big = 99999999999
+print(small , big)
+#二进制
+bv = 0b10101
+print(bv)
+#八进制
+ov = 0o275
+print(ov)
+#十六进制
+xv = 0x1af
+print(xv)
+#float
+fv1 = 100.2
+fv2 = -2.5
+fv3 = 3.4e4
+print(fv1 , fv2 ,fv3)
+#complex number
+#cmath函数包含了一系列的复数运算
+c1 = 2 + 4j
+c2 = 3.3 - 2.3j
+print(c1, c2)
+print(c1 + c2)
+print(type(c1))
--- ex5.py
+++ ex5.py
@@ -6,9 +6,9 @@ my_eyes = "Black"
 my_teeth = "White"
 my_hair = "Black"
 
-print "Let's talk about %s ." % my_name
-print "He is %d inches tall." % my_height
-print "He is %d pounds heavy." % my_weight
-print "He got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His tooth are usually %s depending on the coffee." % my_teeth
-print "If I add %d, %d, %d I get %d." %(my_age, my_height, my_weight, my_age+my_height+my_weight)
+print("Let's talk about %s ." % my_name)
+print("He is %d inches tall." % my_height)
+print("He is %d pounds heavy." % my_weight)
+print("He got %s eyes and %s hair." % (my_eyes, my_hair))
+print("His tooth are usually %s depending on the coffee." % my_teeth)
+print("If I add %d, %d, %d I get %d." %(my_age, my_height, my_weight, my_age+my_height+my_weight))
