d6427296a50e31962ef038d1fe2d22534f910ea4
itmanharry <jack_chengqian@163.com>
2019-12-05 21:20:18 +0800

    string2

--- PythonCSDN/code/string.py
+++ PythonCSDN/code/string.py
@@ -41,3 +41,47 @@ print(b3)
 #使用decode将bytes解码成字符串
 s = b3.decode('utf-8')
 print(s)
+#字符串高级
+s = "a\tb\tc"
+print(s)
+s = "I love %s"
+print(s % "Python")
+s = "I love %s, the price is %d"
+print(s%("Python book",300))
+price = 128
+s = "price is :%d, o value is : %o, x value is : %x, string value is : %r "
+print(s%(price,price,price,price))
+#字符串索引
+s = "IamChengGuoqian"
+print(s[2])
+print(s[2:6])
+print(s[2:7:2])
+#in是否包含
+print('hdd' in s)
+#字符串长度
+print(len(s))
+#取最大最小字符
+print(max(s), min(s))
+#title函数
+s = "abCDEfg"
+print(s.title())
+print(s.lower())
+print(s.upper())
+s = "   Python  "
+print(s.strip())
+print(s.lstrip())
+print(s.rstrip())
+s = 'IamHarry'
+print(s.startswith('I'))
+print(s.endswith('a'))
+print(s.find('m'))
+print(s.find('v'))
+print(s.index('H'))
+#print(s.index('v'))
+s = s.replace('a', 'v')
+print(s)
+print(s.find('v'))
+#print(s.translate())
+s = "a.b.c.d.e"
+print(s.split("."))
+print("|".join(s.split(".")))
