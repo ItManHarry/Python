765102decc28ca6c7623b9fdca4889453e382844
itmanharry <jack_chengqian@163.com>
2019-12-06 21:05:07 +0800

    assgin

---
+++ PythonCSDN/code/assgin.py
@@ -0,0 +1,23 @@
+#普通除法
+div = 5 / 3
+print(div)
+#整除
+div = 5 // 3
+print(div)
+div = 6.5 // 2.3
+print(div)
+#求余的结果正负值始终和除数保持一致
+mod = 23 % 7
+print(mod)
+mod = -23 % 7
+print(mod)
+mod = 23 %  -7
+print(mod)
+mod = -23 % -7
+print(mod)
+#乘方
+print(3 ** 3)
+#开方
+print(27 ** (1/3))
+import math
+print(math.sin(3.14 / 4))
