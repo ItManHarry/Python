b9cbd6ae7c7494f0e7f2c6e591f727b7806c3cff
ddpday-user <ddpday-user@doosan.com>
2020-04-17 10:33:40 +0800

    ddd

--- ex1.py
+++ ex1.py
@@ -8,3 +8,4 @@ print "I'd much rather you 'not'."
 print 'I "said" do not touch this'
 print "I am Harry , now I begain to study the Python language..."
 #Maybe this line won't be printed. Yes it is a comment line
+
